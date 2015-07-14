af4e5c1cc8595f92629e27842d5a1f2b7eccd349
Hesham Amer <HeshamHamdyAmer@gmail.com>
2015-07-14 02:44:27 +0200

    First Branched Code
    Added Collections for match history and recent matches that retrieves
    it's data from the KPIs.
    Fixed _id in the Recent history to be the summoner ID

--- RitoMongo.py
+++ RitoMongo.py
@@ -29,20 +29,38 @@ def getUserInput():
     elif (not FileExists):
         SummonerName= raw_input('Enter your Summoner name: ');
         Region  = (raw_input('Enter your region: ')).upper();
-        Key = raw_input('Enter your API Key which you retrieved from Rito website: ');
+        Key = raw_input('Enter your API Key which you retrieved from Riot website: ');
         f = open('RitoMongo.conf','w');
         f.write(SummonerName+'\n'+Region+'\n'+Key);
         f.close();
     return res;
-        
+
+
+def getMatchHistory(SummonerID):
+
+      mhURL="https://eune.api.pvp.net/api/lol/" + Region.lower() + "/v2.2/matchhistory/" + `SummonerID`+ "?api_key=" + Key;
+      print mhURL;
+      mh_data=getJSONReply(mhURL);
+      print mh_data;
+      
+      return mh_data;
+      
+def getRecentHistory(SummonerID):
+      rURL="https://eune.api.pvp.net/api/lol/" + Region.lower()+ "/v1.3/game/by-summoner/" + `SummonerID`+ "/recent?api_key=" + Key;
+      print rURL;
+      r_data=getJSONReply(rURL);
+      print r_data;
+      r_data['_id']=r_data['summonerId'];
+      r_data.pop('summonerId');
+      return r_data;
 def ReformatJSON(SummonerName,Region,Key):
     idURL = "https://na.api.pvp.net/api/lol/" + Region+ "/v1.4/summoner/by-name/" + SummonerName+ "?api_key=" + Key;
-    uID_data = getJSONReply(idURL);
-    idURL=uID_data[SummonerName.lower()];
-    idURL['_id'] = idURL['id'];
-    idURL.pop('id');
-    print idURL;
-    return idURL,uID_data;
+    id_data = getJSONReply(idURL);
+    idRes=id_data[SummonerName.lower()];
+    idRes['_id'] = idRes['id'];
+    idRes.pop('id');
+    print idRes;
+    return idRes,id_data;
 
 #================================= Main =============================================================================
 _InputFields= getUserInput();
@@ -53,13 +71,18 @@ print "----------------------------------------------------------------------";
 print "If you've inputted wrong data, please delete or modify RitoMongo.conf" ;
 print "----------------------------------------------------------------------";
 # Retrieving the SummonerID;
-idURL,uID_data=ReformatJSON(SummonerName,Region,Key);
-SummonerID = uID_data[SummonerName.lower()]["_id"];
-print "The printed SummonerID from json is : " ,SummonerID;
+idURL,id_data=ReformatJSON(SummonerName,Region,Key);
+SummonerID = id_data[SummonerName.lower()]["_id"];
 
+mh_data=getMatchHistory(SummonerID);
+rdata=getRecentHistory(SummonerID);
 # Connecting to mongoDB database
 client = MongoClient();
 db = client['RitoMongoDB'];
-collection = db["SummonerID"];
-entryID = collection.insert_one(idURL).inserted_id;
+SummonerID_Collection = db["SummonerID"];
+MatchHistory_Collection = db["MatchHistory"];
+RecentHistory_Collection = db["RecentHistory"];
+#entryID = SummonerID_Collection.insert_one(idURL).inserted_id;
+entryID = MatchHistory_Collection.insert_one(mh_data).inserted_id;
+entryID = RecentHistory_Collection.insert_one(rdata).inserted_id;
 client.close();
